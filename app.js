f996e6cff80499e5b1770451a9ae460ca2eac496
FrLans <supcreerobrinevn@gmail.com>
2025-03-18 23:39:17 +0700

    Function to add new Entries

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 let inpName = document.getElementById("inpName");
+let inpImg = document.getElementById("inpImg");
 let inpAge = document.getElementById("inpAge");
 let inpSpecies = document.getElementById("inpSpecies");
 let inpJob = document.getElementById("inpJob");
 let inpDesc = document.getElementById("inpDesc");
+let main = document.getElementById("main");
 let submit = document.getElementById("submit");
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -22,11 +28,57 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Khởi động: Tải danh sách nhân vật lên
+getDocs(collection(db, "data"))
+  .then((snap) => {
+    // Add từng nhân vật vào từ dữ liệu có sẵn
+    let display = "";
+    snap.forEach((doc) => {
+      display += `      <div class="characterPreview">
+        <img src="${doc.data().img}" desc="${doc.data().desc}"/>
+        <div class="charStatName">
+          <p class="charName">Tên</p>
+          <p class="charAge">Tuổi</p>
+          <p class="charSpecies">Chủng tộc</p>
+          <p class="charJob">Công việc</p>
+        </div>
+        <div class="charInfo">
+          <p class="charName">"${doc.data().name}"</p>
+          <p class="charAge">"${doc.data().age}"</p>
+          <p class="charSpecies">"${doc.data().species}"</p>
+          <p class="charJob">"${doc.data().job}"</p>
+        </div>
+      </div>`;
+    });
+    // Load đống dữ liệu ra
+    main.innerHTML = display;
+  })
+  .catch((error) => alert(error));
 
 submit.addEventListener("click", function () {
-  localStorage.setItem("tempName", inpName.value);
-  localStorage.setItem("tempAge", inpAge.value);
-  localStorage.setItem("tempSpecies", inpSpecies.value);
-  localStorage.setItem("tempJob", inpJob.value);
-  localStorage.setItem("tempImg");
+  if (
+    !inpName.value.trim() ||
+    !inpImg.value.trim() ||
+    !inpAge.value.trim() ||
+    !inpSpecies.value.trim() ||
+    !inpJob.value.trim() ||
+    !inpDesc.value.trim()
+  ) {
+    alert("Xin hãy nhập đầy đủ dữ liệu về nhân vật");
+  } else {
+    addDoc(collection(db, "data"), {
+      img: inpImg.value.trim(),
+      name: inpName.value.trim(),
+      age: inpAge.value.trim(),
+      species: inpSpecies.value.trim(),
+      job: inpJob.value.trim(),
+      desc: inpDesc.value.trim(),
+    })
+      .then(() => {
+        window.location.reload();
+      })
+      .catch((error) => alert(error));
+  }
 });
