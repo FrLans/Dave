f2095eee9ee75b99310d780fedabfcf14614d310
FrLans <supcreerobrinevn@gmail.com>
2025-03-23 20:57:44 +0700

    Finalize 2

--- index.js
+++ index.js
@@ -127,3 +127,20 @@ setTimeout(function () {
 adOff.addEventListener("click", function () {
   popUp.style.display = "none";
 });
+
+let beg = document.getElementById("beg");
+let qr = document.getElementById("QR");
+let qrState = false;
+
+beg.addEventListener("click", function () {
+  if (!qrState) {
+    qr.style.display = "block";
+    beg.innerText = "Không ủng hộ nữa";
+    qrState = true;
+  } else if (qrState) {
+    qr.style.display = "none";
+    beg.innerText = "Ủng hộ nhà phát triển Web!";
+    alert(">:(");
+    qrState = false;
+  }
+});
